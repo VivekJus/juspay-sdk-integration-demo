24e1b24e34db3b54972ea9b26bf7956a7b4023ee
VivekJus <162992597+VivekJus@users.noreply.github.com>
2024-10-29 07:14:48 +0530

    Update request.sh

--- api-reference/orders/order-status/v4-order-status/request.sh
+++ api-reference/orders/order-status/v4-order-status/request.sh
@@ -1,6 +1,6 @@
 curl --location 'https://sandbox.juspay.in/v4/order-status' \
 --header 'Content-Type: application/json' \
---header 'x-merchantid: branch' \
+--header 'x-merchantid: *****' \
 --header 'Authorization: Basic ***************************' \
 --data '{
     "JWT": "AddEncryptedPayloadHere****************************************************************************************************************************************************************************************************"
