63e3d7fd1c38381cd34925b8d1b9c86ec3da04f3
VivekJus <162992597+VivekJus@users.noreply.github.com>
2024-10-29 07:07:43 +0530

    Create request.sh for v4 osrder status

---
+++ api-reference/orders/order-status/v4-order-status/request.sh
@@ -0,0 +1,8 @@
+curl --location 'https://sandbox.juspay.in/v4/order-status' \
+--header 'Content-Type: application/json' \
+--header 'x-merchantid: branch' \
+--header 'Authorization: Basic ***************************' \
+--data '{
+    "JWT": "AddEncryptedPayloadHere****************************************************************************************************************************************************************************************************"
+}
+'
